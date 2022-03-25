42bdfae1e1d66a232dd1d57e27e6dd32c3b4caa3
ikeniborn <ikeniborn@gmail.com>
2022-03-25 12:24:10 +0300

    update

--- src/portfolio/worksheet/registry.js
+++ src/portfolio/worksheet/registry.js
@@ -39,34 +39,4 @@ class Registry {
   getRegistry() {
     return Object.values(new Registry().values)
   }
-
-  // updateUsdPerCurrency(editRange) {
-  //   this.eMap = new Map(Object.entries(editRange))
-  //   if (this.eMap.has('range')) {
-  //     if (
-  //       this.eMap.get('range').columnStart === this.head.registry.currency.num
-  //     ) {
-  //       const rowNum = this.eMap.get('range').rowStart
-  //       const rowIndex = rowNum - 2
-  //       const rowValues = this.workSheet.dataValues.filter(
-  //         (row, index) => index === rowIndex
-  //       )[0]
-  //       const currency = rowValues[this.head.registry.currency.idx]
-  //       const time = new utils.FormatNumber(
-  //         rowValues[this.head.registry.time.idx]
-  //       ).getHourAndMinuteFromNumber()
-  //       const dateTime = new utils.FormatDate(
-  //         rowValues[this.head.registry.date.idx]
-  //       ).addTime(time.h, time.m).date
-  //       const price = new cryptoCompare.Price(
-  //         cryptoCompareInstance
-  //       ).getHistoryPrice(currency, dateTime)[currency.toUpperCase()].USD
-  //       this.workSheet.portfolio.registry.insertValue(
-  //         price,
-  //         rowNum,
-  //         this.head.registry.usdPerCurrency.num
-  //       )
-  //     }
-  //   }
-  // }
 }
