c299c144396bd0846d3f05c907247566dc2e3584
ikeniborn <ikeniborn@gmail.com>
2024-03-18 08:40:55 +0300

    up

--- app/web/app/func/google.py
+++ app/web/app/func/google.py
@@ -3,6 +3,8 @@ from gspread import Worksheet, Spreadsheet
 import streamlit as st
 from func.duckdb import DuckDb
 from pandas import DataFrame
+# from gcloud import storage
+# from oauth2client.service_account import ServiceAccountCredentials
 
 class GoogleSpreadsheet:
     'Класс работы с табилцами гугл'
@@ -60,3 +62,27 @@ class GoogleWorksheet():
   
   def insert(self, dataframe):
     self.db.insert(dataframe,self.worksheet_name)
+    
+# class GoogleStorage:
+#     'Класс работы с табилцами гугл'
+#     def __new__(cls, *args, **kwargs):
+#       return super().__new__(cls)
+          
+#     def __init__(self,bucket_name,credential) -> None:
+#       self.credential=credential
+#       self.storage=None
+
+#     def get_storage(self,ttl=3600):
+#       @st.cache_resource(ttl=ttl)
+#       def _get_storage(credential) -> Spreadsheet:
+#         credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+#             credential
+#         )
+#         client = storage.Client(credentials=credentials)
+#         return client
+#       self.client = _get_storage(self.bucket_name,self.credential)
+#       bucket = self.client.get_bucket('mybucket')
+#       blob = bucket.blob('myfile')
+#       blob.upload_from_filename('myfile')
+#       return self
+    
