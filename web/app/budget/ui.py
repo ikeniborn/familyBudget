fd7ed7f20b3ea9fc7102bf1bace171733197a334
ikeniborn <ikeniborn@gmail.com>
2024-11-21 20:08:04 +0300

    гз

--- web/app/budget/ui.py
+++ web/app/budget/ui.py
@@ -63,7 +63,6 @@ if __name__ == "__main__":
 
     telegram_login = TelegramLoginWidgetComponent(**config.config)
 
-
     # if not st.session_state["username"]:
     if "telegramm_session" not in st.session_state:
         value = telegram_login.button
