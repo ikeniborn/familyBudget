26dbce97933ec82aac73358ff871205ad539a7c0
ikeniborn <ikeniborn@gmail.com>
2024-11-21 18:43:52 +0300

    up

--- web/app/budget/ui.py
+++ web/app/budget/ui.py
@@ -64,12 +64,13 @@ if __name__ == "__main__":
     
     # if not st.session_state["username"]:
     value = telegram_login.button
+    st.write(value)
     if value:
         if "telegramm_session" not in st.session_state.telegramm_session:
             st.session_state.telegramm_session = value
                 
-    else:
-        st.write(st.session_state.telegramm_session)
+    # else:
+        # st.write(st.session_state.telegramm_session)
     #     users = Users().fetchAll(ttl=86400)
     #     if "user_key" not in st.session_state:
     #         st.session_state.user_key = users.lazy().filter(pl.col(["user_name"]) == st.session_state.username).collect()["user_key"][0]               
