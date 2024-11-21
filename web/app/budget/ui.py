5e99bc299edb3a761af8277439524f86e2eb04ff
ikeniborn <ikeniborn@gmail.com>
2024-11-21 19:17:10 +0300

    up

--- web/app/budget/ui.py
+++ web/app/budget/ui.py
@@ -64,57 +64,54 @@ if __name__ == "__main__":
     telegram_login = TelegramLoginWidgetComponent(**config.config)
 
     # if not st.session_state["username"]:
-    value = telegram_login.button
-
-    if value:
-        if "telegramm_session" not in st.session_state:
+    if "telegramm_session" not in st.session_state:
+        value = telegram_login.button
+        if value:
             st.session_state.telegramm_session = value
             users = Users().fetchAll(ttl=86400)
             if "user_key" not in st.session_state:
                 st.session_state.user_key = users.lazy().filter(pl.col(["user_name"]) == st.session_state.telegramm_session["username"]).collect()["user_key"][0]
-        else:
-            users = Users().fetchAll(ttl=86400)
-            if "user_key" not in st.session_state:
-                st.session_state.user_key = users.lazy().filter(pl.col(["user_name"]) == st.session_state.telegramm_session["username"]).collect()["user_key"][0]
-                financial_centers = FinancialCenters().fetchAll(ttl=86400)
-                cost_centers = CostCenter().fetchAll(ttl=86400)
-                nomenclatures = Nomenclatures().fetchAll(ttl=86400)
-                row_types = RowTypes().fetchAll(ttl=86400)
 
-                st.sidebar.title(f"Привет {st.session_state.first_name}")
-                clicked = st.sidebar.button("Выход")
-                if clicked:
-                    telegram_login.clear_session()
-                    # st.rerun()
+    if "user_key" in st.session_state:
+        financial_centers = FinancialCenters().fetchAll(ttl=86400)
+        cost_centers = CostCenter().fetchAll(ttl=86400)
+        nomenclatures = Nomenclatures().fetchAll(ttl=86400)
+        row_types = RowTypes().fetchAll(ttl=86400)
 
-                fact, budget, report = st.tabs(["Факт", "Бюджет", "Отчетность"])
+        st.sidebar.title(f"Привет {st.session_state.first_name}")
+        clicked = st.sidebar.button("Выход")
+        if clicked:
+            telegram_login.clear_session()
+            # st.rerun()
 
-                with fact:
-                    Forms.Fact(
-                        financial_centers=financial_centers,
-                        cost_centers=cost_centers,
-                        row_types=row_types,
-                        nomenclatures=nomenclatures,
-                    ).form()
+        fact, budget, report = st.tabs(["Факт", "Бюджет", "Отчетность"])
 
-                with budget:
-                    Forms.Budget(
-                        financial_centers=financial_centers,
-                        cost_centers=cost_centers,
-                        row_types=row_types,
-                        nomenclatures=nomenclatures,
-                    ).form()
+        with fact:
+            Forms.Fact(
+                financial_centers=financial_centers,
+                cost_centers=cost_centers,
+                row_types=row_types,
+                nomenclatures=nomenclatures,
+            ).form()
 
-                with report:
-                    Forms.Report(
-                        financial_centers=financial_centers,
-                        cost_centers=cost_centers,
-                        row_types=row_types,
-                        nomenclatures=nomenclatures,
-                    ).report()
-            else:
-                telegram_login.clear_session()
-                st.rerun()
+        with budget:
+            Forms.Budget(
+                financial_centers=financial_centers,
+                cost_centers=cost_centers,
+                row_types=row_types,
+                nomenclatures=nomenclatures,
+            ).form()
+
+        with report:
+            Forms.Report(
+                financial_centers=financial_centers,
+                cost_centers=cost_centers,
+                row_types=row_types,
+                nomenclatures=nomenclatures,
+            ).report()
+    else:
+        telegram_login.clear_session()
+        st.rerun()
 
         # st.write(telegram_login.get_session)
 
