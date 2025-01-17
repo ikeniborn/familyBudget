d4c95ee315a9d1e95ff12472485867a3d7d30f53
ikeniborn <ikeniborn@gmail.com>
2025-01-17 18:08:43 +0300

    up

--- web/db/postgresql/backup/postgres-backup.sh
+++ web/db/postgresql/backup/postgres-backup.sh
@@ -9,10 +9,10 @@ fi
 sudo docker exec -t postgres pg_dumpall -c -U postres | gzip > $BACKUP_NAME
 
 if [[ -f $BACKUP_NAME ]]; then
-  ~/minio-binaries/mc cp $BACKUP_NAME yandex/ikeniborn-web-postgresql
+  /root/minio-binaries/mc cp $BACKUP_NAME yandex/ikeniborn-web-postgresql
   if [[ -f $BACKUP_NAME_OLD ]]; then
   sudo rm $BACKUP_NAME_OLD 
-  ~/minio-binaries/mc rm yandex/ikeniborn-web-postgresql/$BACKUP_NAME_OLD
+  /root/minio-binaries/mc rm yandex/ikeniborn-web-postgresql/$BACKUP_NAME_OLD
   fi
 fi
 
