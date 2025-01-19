a48dab2a08ac1302ea364f5a7d213e4cc66872a6
ikeniborn <ikeniborn@gmail.com>
2025-01-19 16:43:51 +0300

    up

--- web/app/budget/ui.py
+++ web/app/budget/ui.py
@@ -9,7 +9,6 @@ import polars as pl
 import os
 from streamlit_telegram_login import TelegramLoginWidgetComponent
 from streamlit_telegram_login.helpers import YamlConfig
-from utils.api import Periods, Registry, Report
 
 # from polars import DataFrame
 
@@ -49,11 +48,7 @@ from utils.forms import Forms
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Домашний бюджет", page_icon=":book", initial_sidebar_state="auto", layout="wide")
-    
-    t_d_period = Periods().fetchMany(
-                start_date=Functions.get_period(shuffle=-1), end_date=Functions.get_period(shuffle=0), ttl=60
-            )
-    st.write(t_d_period)
+
 
     # users = Users().fetchAll(ttl=1)
     # st.write(users.select("user_name").filter(pl.col("user_name") == "ilya").unique().sort(by="user_name")["user_name"][0])
@@ -99,13 +94,13 @@ if __name__ == "__main__":
 
         fact, budget, report = st.tabs(["Факт", "Бюджет", "Отчетность"])
 
-        with fact:
-            Forms.Fact(
-                financial_centers=financial_centers,
-                cost_centers=cost_centers,
-                row_types=row_types,
-                nomenclatures=nomenclatures,
-            ).form()
+        # with fact:
+        #     Forms.Fact(
+        #         financial_centers=financial_centers,
+        #         cost_centers=cost_centers,
+        #         row_types=row_types,
+        #         nomenclatures=nomenclatures,
+        #     ).form()
 
         with budget:
             Forms.Budget(
@@ -115,13 +110,13 @@ if __name__ == "__main__":
                 nomenclatures=nomenclatures,
             ).form()
 
-        with report:
-            Forms.Report(
-                financial_centers=financial_centers,
-                cost_centers=cost_centers,
-                row_types=row_types,
-                nomenclatures=nomenclatures,
-            ).report()
+        # with report:
+        #     Forms.Report(
+        #         financial_centers=financial_centers,
+        #         cost_centers=cost_centers,
+        #         row_types=row_types,
+        #         nomenclatures=nomenclatures,
+        #     ).report()
 
     # else:
     # telegram_login.clear_session()
