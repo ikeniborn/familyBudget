525be0cb0151cc8b8f3191072e3dbaeda476f326
ikeniborn <ikeniborn@gmail.com>
2025-01-21 00:22:26 +0300

    up

--- web/db/couchdb/backup/couchdb-backup.sh
+++ web/db/couchdb/backup/couchdb-backup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BACKUP_COUCHDB_NAME_OLD=couchdb-$(date -d "2 days ago" +%Y%m%d).tar
 BACKUP_COUCHDB_NAME=couchdb-$(date -u +%Y%m%d).tar
 BACKUP_COUCHDB_CATALOG=/home/ikeniborn/app/web/db/couchdb/backup
@@ -7,6 +7,7 @@ cd $BACKUP_COUCHDB_CATALOG
 if [[ -f $BACKUP_COUCHDB_NAME ]]; then
   sudo rm $BACKUP_COUCHDB_NAME 
 fi
+
 sudo docker run --rm --volume web_couchdb-data:/data --volume $BACKUP_COUCHDB_CATALOG:/backup couchdb tar cvf /backup/$BACKUP_COUCHDB_NAME data
 
 if [[ -f $BACKUP_COUCHDB_NAME ]]; then
