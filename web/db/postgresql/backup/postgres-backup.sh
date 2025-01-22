ae9dd3bdcb2c8fdd67c553d5566907180dfaf21a
ikeniborn <ikeniborn@gmail.com>
2025-01-22 09:39:37 +0300

    up

--- web/db/postgresql/backup/postgres-backup.sh
+++ web/db/postgresql/backup/postgres-backup.sh
@@ -4,15 +4,15 @@ BACKUP_POSTGRES_NAME=postres-$(date -u +%Y%m%d).gz
 BACKUP_POSTGRES_CATALOG=/home/ikeniborn/app/web/db/postgresql/backup
 
 cd $BACKUP_POSTGRES_CATALOG
-if [[ -f $BACKUP_POSTGRES_NAME ]]; then
+if [ -f $BACKUP_POSTGRES_NAME ]; then
   sudo rm $BACKUP_POSTGRES_NAME 
 fi
 
 sudo docker exec -t postgres pg_dumpall -c -U postres | gzip > $BACKUP_POSTGRES_NAME
 
-if [[ -f $BACKUP_POSTGRES_NAME ]]; then
+if [ -f $BACKUP_POSTGRES_NAME ]; then
   sudo /opt/minio-binaries/mc cp $BACKUP_POSTGRES_NAME yandex/ikeniborn-web-postgresql/
-  if [[ -f $BACKUP_POSTGRES_NAME_OLD ]]; then
+  if [ -f $BACKUP_POSTGRES_NAME_OLD ]; then
     sudo rm $BACKUP_POSTGRES_NAME_OLD 
     sudo /opt/minio-binaries/mc rm yandex/ikeniborn-web-postgresql/$BACKUP_POSTGRES_NAME_OLD
   fi
