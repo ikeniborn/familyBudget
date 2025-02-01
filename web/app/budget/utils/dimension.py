c90f76ec66f310a8da41c41bb3a66c35c2b4db2b
ikeniborn <ikeniborn@gmail.com>
2025-02-01 14:40:20 +0300

    up

--- web/app/budget/utils/dimension.py
+++ web/app/budget/utils/dimension.py
@@ -66,9 +66,13 @@ class Dimension:
                 & (pl.col("operation_name") == operation_name)
                 & (pl.col("bill_name") == st.session_state[bill_key])
                 & (pl.col("account_name") == st.session_state[account_key])
-            ).select("nomenclature_name")
+            ).select("nomenclature_name").collect()["nomenclature_name"]
+            
+            st.write(nomenclature_df)
 
             nomenclature_df_count = nomenclature_df.count()
+            
+            st.write(nomenclature_df_count)
 
             if nomenclature_df_count["nomenclature_name"][0] > 1:
                 # TODO Реализовать проверку на количество записей и автоматический выбор если значение равно 1
@@ -94,8 +98,8 @@ class Dimension:
             else:
                 if nomenclature_key not in st.session_state:
                     st.session_state[nomenclature_key] = nomenclature_df["nomenclature_name"][0]
-                # else:
-                    # st.session_state[nomenclature_key] = nomenclature_df["nomenclature_name"][0]
+                else:
+                    st.session_state[nomenclature_key] = nomenclature_df["nomenclature_name"][0]
             st.write(st.session_state[nomenclature_key])
 
             if st.session_state[account_key] and st.session_state[nomenclature_key]:
