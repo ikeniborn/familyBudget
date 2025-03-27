67414d82e4fbd924b40b606cff4cbe7dba79a9ba
ikeniborn <ikeniborn@gmail.com>
2025-03-27 16:03:26 +0300

    up

--- web/db/postgresql/ddl/budgetdb.sql
+++ web/db/postgresql/ddl/budgetdb.sql
@@ -13,20 +13,26 @@ DROP TABLE if EXISTS public.t_d_row_type CASCADE;
 DROP TABLE if EXISTS public.t_d_user CASCADE;
 
 -- public.t_d_cost_center definition
-drop SEQUENCE t_d_cost_center_cost_center_id_seq;
+drop SEQUENCE if EXISTS t_d_cost_center_cost_center_id_seq;
 CREATE SEQUENCE t_d_cost_center_cost_center_id_seq as BIGINT INCREMENT BY 1 MINVALUE 1 START 100 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_cost_center (
   cost_center_id bigint NOT NULL DEFAULT nextval('t_d_cost_center_cost_center_id_seq'),
   cost_center_name varchar NOT NULL,
-  created_dttm timestamp DEFAULT now() NOT NULL,
+  created_dttm timestamp DEFAULT now() NOT NULL COMMENT,
   updated_dttm timestamp DEFAULT now() NOT NULL,
   CONSTRAINT t_d_cost_center_cost_center_name_unique UNIQUE (cost_center_name),
   CONSTRAINT t_d_cost_center_pk PRIMARY KEY (cost_center_id)
 );
+comment on column t_d_cost_center.cost_center_id is 'Идентификатор МВЗ';
+comment on column t_d_cost_center.cost_center_name is 'Наименование МВЗ';
+comment on column t_d_cost_center.created_dttm is 'Дата создания';
+comment on column t_d_cost_center.updated_dttm is 'Дата обновления';
+comment on table t_d_cost_center is 'Справочник МВЗ (место возникновения затрат)';
+
 
 -- public.t_d_financial_center definition
-drop SEQUENCE t_d_financial_center_financial_center_id_seq;
+drop SEQUENCE if EXISTS t_d_financial_center_financial_center_id_seq;
 CREATE SEQUENCE t_d_financial_center_financial_center_id_seq as BIGINT INCREMENT BY 1 MINVALUE 1 START 100 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_financial_center (
@@ -37,9 +43,14 @@ CREATE TABLE public.t_d_financial_center (
   CONSTRAINT t_d_financial_center_financial_center_name_unique UNIQUE (financial_center_name),
   CONSTRAINT t_d_financial_center_pk PRIMARY KEY (financial_center_id)
 );
+comment on column t_d_financial_center.financial_center_id is 'Идентификатор ЦФО';
+comment on column t_d_financial_center.financial_center_name is 'Наименование ЦФО';
+comment on column t_d_financial_center.created_dttm is 'Дата создания';
+comment on column t_d_financial_center.updated_dttm is 'Дата обновления';
+comment on table t_d_financial_center is 'Справочник ЦФО (центр финансовой ответсвенности)';
 
 -- public.t_d_nomenclature definition
-drop SEQUENCE t_d_nomenclature_nomenclature_id_seq;
+drop SEQUENCE if EXISTS t_d_nomenclature_nomenclature_id_seq;
 CREATE SEQUENCE t_d_nomenclature_nomenclature_id_seq as bigint INCREMENT BY 1 MINVALUE 1 START 100 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_nomenclature (
@@ -55,9 +66,19 @@ CREATE TABLE public.t_d_nomenclature (
   CONSTRAINT t_d_nomenclature_nomenclature_name_unique UNIQUE (nomenclature_name),
   CONSTRAINT t_d_nomenclature_pk PRIMARY KEY (nomenclature_id)
 );
+comment on column t_d_nomenclature.nomenclature_id is 'Идентификатор номенклатуры';
+comment on column t_d_nomenclature.nomenclature_name is 'Наименование номенклатуры';
+comment on column t_d_nomenclature.account_name is 'Наименование статьи';
+comment on column t_d_nomenclature.bill_name is 'Наименование сцета';
+comment on column t_d_nomenclature.operation_name is 'Наименование операциии';
+comment on column t_d_nomenclature.is_budget is 'Признак бюджета';
+comment on column t_d_nomenclature.is_fact is 'Признак факта';
+comment on column t_d_nomenclature.created_dttm is 'Дата создания';
+comment on column t_d_nomenclature.updated_dttm is 'Дата обновления';
+comment on table t_d_nomenclature is 'Справочник номенклатур';
 
 -- public.t_d_period definition
-drop SEQUENCE t_d_period_period_id_seq;
+drop SEQUENCE if EXISTS t_d_period_period_id_seq;
 CREATE SEQUENCE t_d_period_period_id_seq as bigint INCREMENT BY 1 MINVALUE 1 START 100 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_period (
@@ -69,9 +90,15 @@ CREATE TABLE public.t_d_period (
   CONSTRAINT t_d_period_period_ru_name_unique UNIQUE (period_ru_name),
   CONSTRAINT t_d_period_pk PRIMARY KEY (period_id)
 );
+comment on column t_d_period.period_id is 'Идентификатор периода';
+comment on column t_d_period.period_dt is 'Дата в форме YYYY-MM-DD';
+comment on column t_d_period.period_ru_name is 'Наименование периода в формате янв 2025';
+comment on column t_d_period.created_dttm is 'Дата создания';
+comment on column t_d_period.updated_dttm is 'Дата обновления';
+comment on table t_d_period is 'Справочник периодов';
 
 -- public.t_d_row_type definition
-drop SEQUENCE t_d_row_type_row_type_id_seq;
+drop SEQUENCE if EXISTS t_d_row_type_row_type_id_seq;
 CREATE SEQUENCE t_d_row_type_row_type_id_seq as bigint INCREMENT BY 1 MINVALUE 1 START 100 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_row_type (
@@ -82,9 +109,14 @@ CREATE TABLE public.t_d_row_type (
   CONSTRAINT t_d_row_type_row_type_name_unique UNIQUE (row_type_name),
   CONSTRAINT t_d_row_type_pk PRIMARY KEY (row_type_id)
 );
+comment on column t_d_row_type.row_type_id is 'Идентификатор типа операции';
+comment on column t_d_row_type.row_type_name is 'Наименование типа операции';
+comment on column t_d_row_type.created_dttm is 'Дата создания';
+comment on column t_d_row_type.updated_dttm is 'Дата обновления';
+comment on table t_d_row_type is 'Справочник типов операций';
 
 -- public.t_d_user definition
-drop SEQUENCE t_d_user_user_id_seq;
+drop SEQUENCE if EXISTS t_d_user_user_id_seq;
 CREATE SEQUENCE t_d_user_user_id_seq as bigint INCREMENT BY 1 MINVALUE 1 START 1 CACHE 1 NO CYCLE;
 
 CREATE TABLE public.t_d_user (
@@ -101,9 +133,20 @@ CREATE TABLE public.t_d_user (
   CONSTRAINT t_d_user_user_login_unique UNIQUE (user_login),
   CONSTRAINT t_d_user_pk PRIMARY KEY (user_id)
 );
+comment on column t_d_user.user_id is 'Идентификатор пользователя';
+comment on column t_d_user.user_name is 'Имя пользователя';
+comment on column t_d_user.user_login is 'Логин пользователя';
+comment on column t_d_user.user_password is 'Пароль пользователя';
+comment on column t_d_user.user_email is 'Электронная почта пользователя';
+comment on column t_d_user.user_google_email is 'Электронная почта Google пользователя';
+comment on column t_d_user.user_telegram_username is 'Имя пользователя telegtam';
+comment on column t_d_user.user_telegram_id is 'Идентификатор пользователя telegtam';
+comment on column t_d_user.created_dttm is 'Дата создания';
+comment on column t_d_user.updated_dttm is 'Дата обновления';
+comment on table t_d_user is 'Справочник пользователей';
 
 -- public.t_f_registry definition
-drop SEQUENCE t_f_registry_registry_id_seq;
+drop SEQUENCE if EXISTS t_f_registry_registry_id_seq;
 CREATE SEQUENCE t_f_registry_registry_id_seq as bigint INCREMENT BY 1 MINVALUE 1 START 1 CACHE 5 NO CYCLE;
 
 CREATE TABLE if not EXISTS public.t_f_registry (
@@ -121,6 +164,19 @@ CREATE TABLE if not EXISTS public.t_f_registry (
   updated_dttm timestamp NOT NULL DEFAULT now(),
   CONSTRAINT t_f_registry_pk PRIMARY KEY (registry_id, operation_dttm)
 ) PARTITION BY range (operation_dttm);
+comment on column t_f_registry.registry_id is 'Идентификатор операции';
+comment on column t_f_registry.operation_dttm is 'Дата и время операции';
+comment on column t_f_registry.period_id is 'Идентификатор операции';
+comment on column t_f_registry.financial_center_id is 'Идентификатор ЦФО';
+comment on column t_f_registry.cost_center_id is 'Идентификатор МВЗ';
+comment on column t_f_registry.nomenclature_id is 'Идентификатор номенклатуры';
+comment on column t_f_registry.cost_sum is 'Сумма операции';
+comment on column t_f_registry.comment_description is 'Комментариц к операции';
+comment on column t_f_registry.row_type_id is 'Идентификатор типа операции';
+comment on column t_f_registry.user_id is 'Идентификатор пользователя';
+comment on column t_f_registry.created_dttm is 'Дата создания';
+comment on column t_f_registry.updated_dttm is 'Дата обновления';
+comment on table t_f_registry is 'Факт операций';
 
 create index t_f_registry_operation_dttm_index on public.t_f_registry using btree (operation_dttm);
 create index t_f_registry_registry_id_index on public.t_f_registry using btree (registry_id);
