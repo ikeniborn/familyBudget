9748292a76d1d46b0756fa74f46cb3cb8b37abda
Ikeniborn <ikeniborn@gmail.com>
2025-07-06 15:34:02 +0300

    РЕактринг бюджета семь на нодjs

--- google/src/_deprecated/telegram/js/library.js
+++
@@ -1,8 +0,0 @@
-/**
- * Initialization Telegram API
- * @param {string} token botId:token
- * @returns {telegramLib}
- */
-function newInstance(token) {
-  new Instance(token)
-}
--- google/src/_deprecated/trello/js/library/instance.js
+++
@@ -1,8 +0,0 @@
-/**
- * Initialization trello API
- * @param {string} key
- * @param {string} token
- */
-function newInstance(key, token) {
-  new Instance(key, token)
-}
